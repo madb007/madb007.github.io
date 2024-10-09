e883ac8c3e2107bb7759131f9e54b156f9726998
madb007 <bhuvanmadala@gmail.com>
2024-10-09 01:28:11 -0500

    resize images

--- personal-site/src/components/ProjectView.vue
+++ personal-site/src/components/ProjectView.vue
@@ -1,96 +1,87 @@
 <template>
-    <div class="min-h-screen bg-gray-900 text-white py-12">
-        <div class="container mx-auto px-4">
-            <h2 class="text-4xl font-bold mb-8 text-center text-blue-400">My Projects</h2>
+  <div class="min-h-screen bg-gray-900 text-white py-12">
+    <div class="container mx-auto px-4">
+      <h2 class="text-4xl font-bold mb-8 text-center text-blue-400">My Projects</h2>
 
-            <div class="space-y-12">
-                <div class="flex flex-col md:flex-row  bg-gray-900 rounded-lg overflow-hidden">
-                    <div class="md:w-1/2 p-6">
-                        <h3 class="text-2xl font-semibold mb-4 text-blue-400">Algo Adversaries</h3>
-                        <p class="text-gray-300 mb-4 text-left">
-                            AlgoAdversaries is a sophisticated full-stack coding challenge platform. Built with a modern
-                            tech stack including React, Tailwind, Node.js, Express, and PostgreSQL. Key features include:
-                            <br><br>
-                                • Code editor with real-time execution and response<br>
-                                • Diverse array of programming challenges across multiple difficulty levels<br>
-                                • Secure user authentication and personalized progress tracking<br>
-                                • RESTful API for seamless front-end and back-end communication
-                        </p>
-                        <a href="https://algo-adversaries.vercel.app"
-                            class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View
-                            Project</a> &nbsp; &nbsp; &nbsp;
-                        <a href="https://github.com/madb007/AlgoAdversaries"
-                                        class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View
- Code</a>
-                    </div>
-                    <div class="md:w-1/2 border-4 border-blue-400 rounded-lg overflow-hidden">
-                        <img src="Project1.jpg" alt="Project 1" class="w-full h-75 object-cover">
-                    </div>
-                </div>
-            <div class="space-y-12">
-                <div class="flex flex-col md:flex-row  bg-gray-900 rounded-lg overflow-hidden">
-                    <div class="md:w-1/2 p-6">
-                        <h3 class="text-2xl font-semibold mb-4 text-blue-400">Trading Engine</h3>
-                        <p class="text-gray-300 mb-4 text-left">
-The Trading Engine Server is a high-performance, scalable system simulating real-time financial market operations. Built with C# and leveraging advanced software engineering principles, it demonstrates expertise in systems design and financial modeling. Key features include:
-                            <br><br>
-                                • Efficient order book management using optimized data structures (SortedSet, ConcurrentDictionary)<br>
-                                • Multiple matching algorithms (FIFO and Pro-rata) for flexible trade execution<br>
-                                • Modular architecture utilizing SOLID principles, dependency injection, and design patterns<br>
-                                • Custom logging system for comprehensive system monitoring and debugging <br>
-                • High-performance, multi-threaded processing of diverse financial instruments and order execution across multiple asset classes <br>
-                • Comprehensive unit and integration testing suite ensuring system reliability
-
-                        </p>
-                        <a href="https://github.com/madb007/TradingEngine"
-                                        class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View
- Code</a>
-                    </div>
-                    <div class="md:w-1/2 border-4 border-blue-400 rounded-lg overflow-hidden">
-                        <img src="Project3.png" alt="Project 3" class="w-full h-75 object-cover">
-                    </div>
-                </div>
-
-
-            <div class="space-y-12">
-                <div class="flex flex-col md:flex-row  bg-gray-900 rounded-lg overflow-hidden">
-                    <div class="md:w-1/2 p-6">
-                        <h3 class="text-2xl font-semibold mb-4 text-blue-400">Multi-Method Option Pricer</h3>
-                        <p class="text-gray-300 mb-4 text-left">
-                Multi-Method Option Pricer is a sophisticated financial tool combining advanced computational methods with a user-friendly interface. Built with C++ and wxWidgets, it demonstrates skill in quantitative finance and GUI development. Key features include: 
-                <br><br>
-
-                • Monte Carlo simulation and Black-Scholes model for option pricing and Greeks calculation <br>
-                • Multi-threaded implementation for high-performance processing of large sample sizes<br>
-                • Custom C++ backend for efficient numerical computations<br>
-                • User-friendly wxWidgets GUI for parameter input and result visualization<br>
-              </p>
-                        <a href="https://github.com/madb007/MC-Options"
-                                        class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View
- Code</a>
-                    </div>
-                    <div class="md:w-1/2 border-4 border-blue-400 rounded-lg overflow-hidden">
-                        <img src="Project2.png" alt="Project 2" class="w-full h-75 object-cover">
-                    </div>
-                </div>
-                <!-- <div class="flex flex-col md:flex-row items-center bg-gray-800 rounded-lg overflow-hidden">
-                    <div class="md:w-2/3 p-6">
-                        <h3 class="text-2xl font-semibold mb-4 text-blue-300">Project Name 2</h3>
-                        <p class="text-gray-300 mb-4">
-                            Description of your second project. Highlight its unique aspects,
-                            your role in it, and any interesting challenges you overcame.
-                        </p>
-                        <a href="#" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View Project</a>
-                    </div>
-                    <div class="md:w-1/3">
-                        <img src="path_to_project2_image.jpg" alt="Project 2" class="w-full h-64 object-cover">
-                    </div>
-                </div> -->
-
-
+      <div class="space-y-12">
+        <div class="flex flex-col md:flex-row bg-gray-900 rounded-lg overflow-hidden">
+          <div class="md:w-1/2 p-6">
+            <h3 class="text-2xl font-semibold mb-4 text-blue-400">Algo Adversaries</h3>
+            <p class="text-gray-300 mb-4 text-left">
+              AlgoAdversaries is a sophisticated full-stack coding challenge platform. Built with a modern
+              tech stack including React, Tailwind, Node.js, Express, and PostgreSQL. Key features include:
+              <br><br>
+              • Code editor with real-time execution and response<br>
+              • Diverse array of programming challenges across multiple difficulty levels<br>
+              • Secure user authentication and personalized progress tracking<br>
+              • RESTful API for seamless front-end and back-end communication
+            </p>
+            <div class="mt-4">
+              <a href="https://algo-adversaries.vercel.app"
+                class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View
+                Project</a>
+              <a href="https://github.com/madb007/AlgoAdversaries"
+                class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 ml-4">View
+                Code</a>
             </div>
+          </div>
+          <div class="md:w-1/2 p-6">
+            <div class="border-4 border-blue-400 rounded-lg overflow-hidden">
+              <img src="Project1.jpg" alt="Project 1" class="w-full h-full object-cover">
+            </div>
+          </div>
         </div>
+
+        <div class="flex flex-col md:flex-row bg-gray-900 rounded-lg overflow-hidden">
+          <div class="md:w-1/2 p-6">
+            <h3 class="text-2xl font-semibold mb-4 text-blue-400">Trading Engine</h3>
+            <p class="text-gray-300 mb-4 text-left">
+              The Trading Engine Server is a high-performance, scalable system simulating real-time financial market operations. Built with C# and leveraging advanced software engineering principles, it demonstrates expertise in systems design and financial modeling. Key features include:
+              <br><br>
+              • Efficient order book management using optimized data structures (SortedSet, ConcurrentDictionary)<br>
+              • Multiple matching algorithms (FIFO and Pro-rata) for flexible trade execution<br>
+              • Modular architecture utilizing SOLID principles, dependency injection, and design patterns<br>
+              • Custom logging system for comprehensive system monitoring and debugging <br>
+              • High-performance, multi-threaded processing of diverse financial instruments and order execution across multiple asset classes <br>
+              • Comprehensive unit and integration testing suite ensuring system reliability
+            </p>
+            <div class="mt-4">
+              <a href="https://github.com/madb007/TradingEngine"
+                class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View
+                Code</a>
+            </div>
+          </div>
+          <div class="md:w-1/2 p-6">
+            <div class="border-4 border-blue-400 rounded-lg overflow-hidden">
+              <img src="Project3.png" alt="Project 3" class="w-full h-full object-cover">
+            </div>
+          </div>
+        </div>
+
+        <div class="flex flex-col md:flex-row bg-gray-900 rounded-lg overflow-hidden">
+          <div class="md:w-1/2 p-6">
+            <h3 class="text-2xl font-semibold mb-4 text-blue-400">Multi-Method Option Pricer</h3>
+            <p class="text-gray-300 mb-4 text-left">
+              Multi-Method Option Pricer is a sophisticated financial tool combining advanced computational methods with a user-friendly interface. Built with C++ and wxWidgets, it demonstrates skill in quantitative finance and GUI development. Key features include: 
+              <br><br>
+              • Monte Carlo simulation and Black-Scholes model for option pricing and Greeks calculation <br>
+              • Multi-threaded implementation for high-performance processing of large sample sizes<br>
+              • Custom C++ backend for efficient numerical computations<br>
+              • User-friendly wxWidgets GUI for parameter input and result visualization<br>
+            </p>
+            <div class="mt-4">
+              <a href="https://github.com/madb007/MC-Options"
+                class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">View
+                Code</a>
+            </div>
+          </div>
+          <div class="md:w-1/2 p-6">
+            <div class="border-4 border-blue-400 rounded-lg overflow-hidden">
+              <img src="Project2.png" alt="Project 2" class="w-full h-full object-cover">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
   </div>
 </template>
